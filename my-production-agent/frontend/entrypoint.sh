685df0cd172543374e1ff0d833e15d1ec9f416f3
Nguyen Nang Anh <nguyennanganh199203@gmail.com>
2026-04-17 19:45:00 +0700

    Final infrastructure fix: Multiple DNS resolvers and simplified entrypoint

--- my-production-agent/frontend/entrypoint.sh
+++ my-production-agent/frontend/entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-# Get the first nameserver from resolv.conf
-NAMESERVER=$(grep nameserver /etc/resolv.conf | head -n 1 | awk '{print $2}')
-# Replace the placeholder in the config
-sed -i "s/DNS_RESOLVER_PLACEHOLDER/$NAMESERVER/g" /etc/nginx/nginx.conf
-# Start Nginx
+# Start Nginx directly - we now use a static list of resolvers in the config
 nginx -g "daemon off;"
